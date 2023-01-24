3f6c2e9d6c10a98869a990ac463bd667db0ed84f
Mennuti Jeronimo <jeromennuti@gmail.com>
2023-01-24 19:28:20 -0300

    agrego botones

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
-
+import freecodecamplogo from './imagenes/FreeCodeCampLogo.png'
+import Boton from './componentes/Boton'
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div cassName='freecodecamp-logo-contenedor'>
+<img 
+src={freecodecamplogo}
+className='freecodecamp-logo'
+alt='Logo de freeCodeCamp' />
+<div className='contenedor-calculadora'></div>
+<div className='fila'><boton>1</boton></div>
+<div className='fila'></div>
+<div className='fila'></div>
+<div className='fila'></div>
+<div className='fila'></div>
+      </div>
     </div>
   );
 }
